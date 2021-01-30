a301099a05f1813e0ac1a0e2e975d046749f9ef1
Chizim <garrohat@yahoo.co.uk>
2021-01-30 18:43:02 +0000

    Completed level 100

--- script.js
+++ script.js
@@ -5,8 +5,25 @@ function setup() {
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const container = document.getElementById("container");
+  episodeList.forEach(episode => {
+    container.innerHTML += `<div><h2>${episode.name} - S${zeroPadded(episode.season)} E${zeroPadded(episode.number)} </h2>
+    <img src= "${episode.image.medium}" alt "">${episode.summary}</div>`;
+    
+// function adds "0" to number to give it a double digit // 
+  function zeroPadded(episodeCode) {
+  return episodeCode.toString().padStart(2, 0); 
 }
+})
+};
+  
+  //  console.log(episode.name);
+  //  console.log(episode.season);
+  //  console.log(episode.number);
+  //  console.log(episode.image.medium)
+  //  console.log(episode.summary);
+  
+  
+
 
 window.onload = setup;
