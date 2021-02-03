19dfbd337f528226d7549e74e21cdacdc509d9a0
Chizim <garrohat@yahoo.co.uk>
2021-02-03 00:00:31 +0000

    Added search bar function

--- script.js
+++ script.js
@@ -1,29 +1,47 @@
 //You can edit ALL of the code here
+const container = document.getElementById("container");
+const searchBar = document.getElementById("searchBar");
+const allEpisodes = getAllEpisodes();
+
 function setup() {
-  const allEpisodes = getAllEpisodes();
+  //  allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
 
-function makePageForEpisodes(episodeList) {
-  const container = document.getElementById("container");
-  episodeList.forEach(episode => {
-    container.innerHTML += `<div><h2>${episode.name} - S${zeroPadded(episode.season)} E${zeroPadded(episode.number)} </h2>
-    <img src= "${episode.image.medium}" alt "">${episode.summary}</div>`;
-    
-// function adds "0" to number to give it a double digit // 
-  function zeroPadded(episodeCode) {
-  return episodeCode.toString().padStart(2, 0); 
-}
-})
-};
-  
-  //  console.log(episode.name);
-  //  console.log(episode.season);
-  //  console.log(episode.number);
-  //  console.log(episode.image.medium)
-  //  console.log(episode.summary);
-  
-  
+// function adds "0" to number to give it a double digit //
+    function zeroPadded(episodeCode) {
+      return episodeCode.toString().padStart(2, 0);
+    }
 
+function helperMarkup (episode) {
+  const markUp = `<div><h2>${episode.name} - S${zeroPadded(
+      episode.season
+    )} E${zeroPadded(episode.number)}</h2>
+    <img src= "${episode.image.medium}" alt "">${episode.summary}</div>`;
+    return markUp;
+}
+
+function makePageForEpisodes(episodeList) {
+  const innerHTMLArray = [];
+  episodeList.forEach((episode) => {
+    innerHTMLArray.push(helperMarkup(episode));
+  });
+  container.innerHTML = innerHTMLArray;
+}
+
+function searchFunction(e){
+  let searchString = e.target.value.toLowerCase();
+      let filteredInput = allEpisodes.filter((char) => {
+        return (
+          char.name.toLowerCase().includes(searchString) ||
+          char.summary.toLowerCase().includes(searchString)
+        );
+      });
+      container.innerHTML = "";
+      makePageForEpisodes(filteredInput);
+      
+}
+
+searchBar.addEventListener("input", searchFunction);
 
 window.onload = setup;
